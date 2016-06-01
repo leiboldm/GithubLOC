f978369be6c72481714e803f75d1f6beb1440aa2
Matt L <leiboldm@umich.edu>
2016-06-01 01:54:54 -0400

    Now displaying lines of code in github page

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 (function(){
-	console.log("This codes runs");
-
 	var valid_file_extensions = ["js", "cpp", "cc", "py", "php", "c", "java", "html", "xml", "json",
 		"swift", "bash", "sh", "pl", "rb", "cs", "m", "h", "hpp", "aspx", "asp", "lua", "prl", "dart"];
 
@@ -12,9 +10,8 @@
 		}
 	}
 
-	function getCount(str, find) {
-		return str.split(find).length - 1;
-	}
+	// mapping from file extension to lines of code
+	var ext_to_count_map = {};
 
 	function httpGetAsync(theUrl, callback)
 	{
@@ -28,32 +25,53 @@
 	    xmlHttp.send(null);
 	}
 
-	// mapping from file extension to lines of code
-	var ext_to_count_map = {};
-
 	function getLocFromUrl(url, file_ext) {
 		function callback(data) {
 			var loc = data.match(/\d+ lines/g);
-			if (loc.length > 1) console.log("Fuck");
+			if (loc.length == 0) {
+				console.log("Fuck");
+				return;
+			}
 			var loc = Number(loc[0].replace("lines", ""));
+			console.log(url + " " + String(loc));
 			addOrCreate(ext_to_count_map, file_ext, loc);
-			console.log(ext_to_count_map);
+			drawLocData();
 		}
 		httpGetAsync(url, callback);
 	}
 
+	function stringifyDict(dict) {
+		var str_arr = [];
+		for (key in dict) {
+			str_arr.push(key + "(" + String(dict[key]) + ")");
+			str_arr.sort();
+		}
+		return str_arr.join(",  ");
+	}
+
+	var display_id = "GithubLOC-loc-display";
+	function drawLocData() {
+		var commit_tease = document.getElementsByClassName("commit-tease")[0];
+		var locDisplay = document.getElementById(display_id);
+		if (!locDisplay) {
+			var locDisplay = document.createElement("div");
+			locDisplay.id = display_id;
+			commit_tease.appendChild(locDisplay);
+		}
+
+		locDisplay.innerHTML = "Lines of code: " + stringifyDict(ext_to_count_map);
+
+	}
+
 	var file_links = document.getElementsByClassName("js-navigation-open");
-	console.log("file_links length = " + String(file_links.length));
 	for (var i = 0; i < file_links.length; i++) {
 		var link = file_links[i];
 		var title = link.title;
-		console.log(link, title);
 		if (title === "" || typeof(title) !== typeof("str")) continue;
 		var file_ext = title.split(".");
 		file_ext = file_ext[file_ext.length - 1];
 		if (valid_file_extensions.indexOf(file_ext) != -1) {
 			var url = link.href;
-			console.log(url, file_ext);
 			getLocFromUrl(url, file_ext);
 		}
 	}
